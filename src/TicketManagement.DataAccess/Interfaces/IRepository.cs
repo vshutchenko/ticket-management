cf467959e01410fe98a3fc9b04d944be9247fa53
Vlad Shutchenko <vshutchenko@mail.ru>
2022-05-16 18:19:21 +0300

    Add generic repository

---
+++ src/TicketManagement.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TicketManagement.DataAccess.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        int Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
