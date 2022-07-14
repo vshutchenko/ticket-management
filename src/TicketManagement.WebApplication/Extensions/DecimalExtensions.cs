f073c9b1caf115535a57bd0f8a049edba9c2f21e
Vlad Shutchenko <vshutchenko@mail.ru>
2022-07-14 17:01:51 +0300

    Fix currency symbol. Change validation error

---
+++ src/TicketManagement.WebApplication/Extensions/DecimalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace TicketManagement.WebApplication.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static string FormatPrice(this decimal price)
+        {
+            var nfi = (NumberFormatInfo)Thread.CurrentThread.CurrentUICulture.NumberFormat.Clone();
+            nfi.CurrencySymbol = "$";
+
+            return string.Format(nfi, "{0:c}", price);
+        }
+    }
+}
